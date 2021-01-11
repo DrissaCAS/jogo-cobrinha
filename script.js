b84db62c1e110810e94d3f91aa7c54e5fda87fbd
Drissa <Drissa_CAS@hotmail.com>
2021-01-10 21:30:49 -0300

    Iniciando Jogo da Cobrinha

---
+++ script.js
@@ -0,0 +1,43 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+
+function criarBG(){
+    context.fillStyle = "violet"; 
+    context.fillRect(0, 0, 16 * box, 16 * box); 
+}
+
+function criarCobrinha(){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "purple";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+let jogo = setInterval(iniciarJogo, 100);
